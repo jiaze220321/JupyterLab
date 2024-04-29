f47a32eab32029d686c9204821118b6ea9e8d87a
jiaze220321 <zjz20190829@163.com>
2024-04-29 01:49:20 +0000

    my-extension index

---
+++ packages/my-extension/src/index.tsx
@@ -0,0 +1,35 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
+/**
+ * 插件激活函数
+ */
+function activate(app: JupyterFrontEnd, mainMenu: IMainMenu): void {
+  const { commands } = app;
+
+  // 添加一个命令
+  const commandId = 'example:open-dialog';
+  commands.addCommand(commandId, {
+    label: 'Say Hello',
+    execute: () => alert('Hello, Jupyter!')
+  });
+
+  // 添加菜单项
+  mainMenu.fileMenu.newMenu.addGroup([{ command: commandId }]);
+}
+
+/**
+ * 插件定义
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'my-extension',
+  autoStart: true,
+  requires: [IMainMenu],
+  activate: activate
+};
+
+export default extension;
